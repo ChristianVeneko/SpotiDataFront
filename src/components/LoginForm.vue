3aa1f3cce07ad90b4b4aa2e7aa41a14a5a8e5045
Christian Mendoza <christianmendozater@gmail.com>
2024-04-20 15:46:33 -0400

    Login Form component

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,9 @@
+<template>
+    <button @click="initiateSpotifyLogin">Iniciar sesión</button>
+  </template>
+  
+  <script setup>
+  const initiateSpotifyLogin = () => {
+    window.location.href = 'http://localhost:3000/login';
+  }
+  </script>
