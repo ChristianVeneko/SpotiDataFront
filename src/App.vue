2d5fb913b3abc486fa7c48fa6d6cbb55c67ff486
Christian Mendoza <christianmendozater@gmail.com>
2024-04-20 15:45:18 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import LoginForm from './components/LoginForm.vue';
+</script>
+
+<template>
+  <div>
+  <LoginForm></LoginForm>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
