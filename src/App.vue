38233a034c4bebd4f399fde6e8671efdfaf0f01f
Christian Mendoza <christianmendozater@gmail.com>
2024-04-21 20:47:02 -0400

    Header Component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,13 @@
-<template>
-  <div id="app">
-    <AuthCallback @updateIsLoggedIn="updateIsLoggedIn" v-if="$route.path === '/auth-callback'" />
-    <h1>{{ appTitle }}</h1>
-    <div v-if="isLoading">Cargando...</div>
-    <div v-else-if="isLoggedIn">
-      <UserInfo :accessToken="accessToken" :refreshAccessToken="reLogin" />
-    </div>
-    <div v-else>
-      <!-- Mostrar LoginForm solo cuando no se ha iniciado sesión -->
-      <LoginForm v-if="!isLoggedIn" />
-    </div>
-  </div>
-</template>
-
 <script setup>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { useAuth } from './useAuth';
+
+//components
 import LoginForm from './components/LoginForm.vue';
 import UserInfo from './components/UserInfo.vue';
 import AuthCallback from './components/AuthCallback.vue';
+import Header from './components/Header.vue';
 const router = useRouter();
 
 
@@ -32,6 +20,20 @@ const updateIsLoggedIn = (newValue) => {
 };
 const reLogin = async () => {
   window.location.href = 'http://localhost:3000/login';
-  console.log('sexooooooooooooooooo')
 }
 </script>
+
+<template>
+  <div id="app">
+    <AuthCallback @updateIsLoggedIn="updateIsLoggedIn" v-if="$route.path === '/auth-callback'" />
+    <Header></Header>
+    <div v-if="isLoading">Cargando...</div>
+    <div v-else-if="isLoggedIn">
+      <UserInfo :accessToken="accessToken" :refreshAccessToken="reLogin" />
+    </div>
+    <div v-else>
+      <!-- Mostrar LoginForm solo cuando no se ha iniciado sesión -->
+      <LoginForm v-if="!isLoggedIn" />
+    </div>
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+      <h1 class="title">Spotify Charts viewer</h1>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
