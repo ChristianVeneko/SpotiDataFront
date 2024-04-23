0c9b561bc69c1b5c93ae445745d8ad340af244c2
Christian Mendoza <christianmendozater@gmail.com>
2024-04-23 00:16:49 -0400

    Import MusicForm And ResultsComponent; getChart function and import Music Class

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,51 @@
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { useAuth } from './useAuth';
+import { Music } from './Music';
 
 //components
 import LoginForm from './components/LoginForm.vue';
 import UserInfo from './components/UserInfo.vue';
 import AuthCallback from './components/AuthCallback.vue';
 import Header from './components/Header.vue';
+import MusicForm from './components/MusicForm.vue';
+import ResultsComponent from './components/ResultsComponent.vue'
+
 const router = useRouter();
 const API_URL = import.meta.env.VITE_API_URL
 
 const appTitle = ref('SpotiData');
+
+const chartData = ref([]);
+const dataType = ref('artists');
+const music = ref(null);
+
+
+
 const { isLoading, isLoggedIn, accessToken } = useAuth();
+
+if(isLoading){
+  music.value = new Music(accessToken);
+}
+
+const getChart = async (data) => {
+  if (data.dataType === 'artists') {
+    dataType.value = 'artist';
+    const topArtists = await music.value.getTopMusic(data.dataTime, data.dataQuantity, data.dataType);
+    chartData.value = topArtists;
+  } else {
+    dataType.value = 'song';
+    const topTracks = await music.value.getTopMusic(data.dataTime, data.dataQuantity, data.dataType);
+    chartData.value = topTracks;
+  }
+};
+
 
 const updateIsLoggedIn = (newValue) => {
   isLoggedIn.value = newValue;
   console.log(isLoading)
 };
+
 const reLogin = async () => {
   window.location.href = `${API_URL}/login`;
 }
@@ -32,6 +61,8 @@ const reLogin = async () => {
       <div id="container">
         <div v-if="isLoading">Cargando...</div>
         <div v-else-if="isLoggedIn">
+        <MusicForm @chartData="getChart"></MusicForm>
+        <ResultsComponent :data="chartData" :type="dataType" />
          <!-- <UserInfo :accessToken="accessToken" :refreshAccessToken="reLogin" /> --> 
         </div>
         <div v-else>
