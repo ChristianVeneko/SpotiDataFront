46f2606321cd2b16794e33b8053ab93e18b82a9c
Christian Mendoza <christianmendozater@gmail.com>
2024-04-22 23:02:07 -0400

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/components/AuthCallback.vue
+++ src/components/AuthCallback.vue
@@ -1,39 +1,35 @@
 <script setup>
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
-
 const API_URL = import.meta.env.VITE_API_URL;
 const router = useRouter();
 const emit = defineEmits(['updateIsLoggedIn']);
 
 onMounted(async () => {
   try {
-    const response = await fetch(`${API_URL}/callback`, {
+    const response = await fetch(`${API_URL}/api/token`, {
       credentials: 'include',
     });
 
     if (response.ok) {
       const data = await response.json();
-      const { access_token, refresh_token, redirect } = data;
+      const accessToken = data.access_token;
 
-      if (access_token) {
-        localStorage.setItem('accessToken', access_token);
-        localStorage.setItem('refreshToken', refresh_token);
-
-        // Redirigir al usuario después de almacenar los tokens
-        window.location.href = redirect;
+      if (accessToken) {
+        localStorage.setItem('accessToken', accessToken);
+        router.push('/');
       } else {
         console.error('No se encontró el token de acceso en la respuesta del servidor');
         emit('updateIsLoggedIn', false);
         router.push('/');
       }
     } else {
-      console.error(`Error al obtener los tokens: ${response.status}`);
+      console.error(`Error al obtener el token de acceso: ${response.status}`);
       emit('updateIsLoggedIn', false);
       router.push('/');
     }
   } catch (error) {
-    console.error('Error al obtener los tokens:', error);
+    console.error('Error al obtener el token de acceso:', error);
     emit('updateIsLoggedIn', false);
     router.push('/');
   }
